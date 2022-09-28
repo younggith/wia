43808ba1e159a6ef9b1f52ebcd17030ff44e82ea
younggi <dudrl6206@naver.com>
2022-09-28 11:51:04 +0900

    first commit

---
+++ src/main/java/com/younggi/wia/WiaApplication.java
@@ -0,0 +1,13 @@
+package com.younggi.wia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younggi/wia/WiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younggi.wia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
