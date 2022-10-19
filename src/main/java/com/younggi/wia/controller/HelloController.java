a7afc41798d814b579416479b40cc94ada2e5b27
younggi <dudrl6206@naver.com>
2022-10-19 16:53:15 +0900

    thymeleaf layout 적용완

--- src/main/java/com/younggi/wia/controller/HelloController.java
+++ src/main/java/com/younggi/wia/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.younggi.wia.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +9,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public String test() {
+        return "index";
+    }
+
     @GetMapping("hello")
     @ResponseBody
     public Hello hello(Hello hello, Model model) {
 //        model.addAttribute("data", "hello다!!");
         return hello;
     }
-
+    @Data
     static class Hello {
         private String name;
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
+        private String title;
     }
 }
